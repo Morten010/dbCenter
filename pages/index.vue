8241c773331e57130a6e3cb62bb90517b0f771f1
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2024-09-26 06:32:59 +0200

    homepage changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,31 @@
-<script setup lang='ts'>
-const containers = await useDocker.getAllContainers()
+<script setup lang="ts">
+const containers = await useDocker.getAllContainers();
 </script>
 
 <!-- homepage -->
 <template>
- <div>
-    <h1>
-      Containers
+  <div 
+    class="w-full flex p-2"
+>
+    <h1 
+        class="text-2xl font-semibold"
+    >
+        Databases
     </h1>
-    
+
     <div>
-      <div
-        v-for="container in containers"
-      >
-        {{ container.Names[0].replace('/', '') }}
+      <div v-for="container in containers">
+        {{ container.Names[0].replace("/", "") }}
       </div>
     </div>
+
+    <button
+        class="fixed bottom-2 right-2 border grid place-content-center rounded hover:opacity-80"
+    >
+        <Icon 
+            name="iconoir:plus" 
+            size="28"
+        />
+    </button>
   </div>
 </template>
