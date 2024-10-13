589d6143e42d59768dcfe8846bde8df81b00b3cd
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2024-10-13 22:04:47 +0200

    windows build test
    assda
    
    remove build thingy
    
    switchin back to module instead of commenjs
    
    lsat try for tonight
    
    dasfaf
    
    webpack config
    
    tdgds
    
    test
    
    Update release.yml
    
    new yml
    
    changes
    
    affsa
    
    Update windows-test.yml
    
    Update windows-test.yml
    
    Update release.yml
    
    test build
    
    update yml workflow
    
    update it
    
    trying github workflow
    
    working on github workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 import { electronConfig } from "./configs/Electron";
+import { extendWebpackConfig } from '@nuxt/kit'
+import esbuild from 'esbuild'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -10,12 +12,34 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     '@vueuse/nuxt',
     '@nuxt/image',
-  ], 
+  ],
   devtools: { enabled: false },
   electron: electronConfig,
   ssr: false,
   alias: {
     pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
+  },
+  build: {
+    extend(config, { isClient, isServer }) {
+      if (isServer) {
+        // Add esbuild plugin for .node files
+        config.plugins.push({
+          name: 'node-file-loader',
+          setup(build) {
+            // Handle .node files as file types
+            build.onResolve({ filter: /\.node$/ }, args => ({
+              path: args.path,
+              namespace: 'node-file'
+            }))
+
+            build.onLoad({ filter: /.*/, namespace: 'node-file' }, async args => ({
+              contents: await fs.promises.readFile(args.path),
+              loader: 'file'
+            }))
+          }
+        })
+      }
+    }
   },
   experimental: {
     appManifest: false,
